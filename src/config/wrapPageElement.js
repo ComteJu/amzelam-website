7fac6006586be2bd3f79d99d2f0574baf4ea2d5b
Ja Comte <lcmte.ju@gmail.com>
2019-02-16 17:58:29 +0100

    some news

--- src/config/wrapPageElement.js
+++ src/config/wrapPageElement.js
@@ -1,22 +1,15 @@
-import React, { PureComponent } from 'react';
-import posed, { PoseGroup } from 'react-pose';
-//import { timeout } from './transition';
+import React, { PureComponent } from "react";
+import posed, { PoseGroup } from "react-pose";
 
 class Transition extends PureComponent {
   render() {
     const { children, location } = this.props;
 
-    //const RoutesContainer2 = posed.div({
-    //  enter: { opacity: 1, delay: timeout, delayChildren: timeout },
-    //  exit: { opacity: 0.7 },
-    //});
     const RoutesContainer = posed.div({
-      enter: {  },
-      exit: {  },
+      enter: {},
+      exit: {}
     });
 
-    // To enable page transitions on mount / initial load,
-    // use the prop `animateOnMount={true}` on `PoseGroup`.
     return (
       <PoseGroup animateOnMount={true}>
         <RoutesContainer key={location.pathname}>{children}</RoutesContainer>
--- src/pages/contact.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import Layout from '../components/layout.js'
-import Discography from '../components/discography'
-export default () => <Layout><Discography/></Layout>
